99f16216343b8f4bbc58d7b86c0d2b7b02a0ca69
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-22 00:00:14 -0500

    - Add custom fonts

--- wellington-limousine/tailwind.config.js
+++ wellington-limousine/tailwind.config.js
@@ -15,6 +15,10 @@ module.exports = {
 	content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
 	theme: {
 		fontSize: fontSizes,
+		fontFamily: {
+			mulish: ["Mulish", "sans-serif"],
+			taviraj: ["taviraj", "serif"],
+		},
 		extend: {},
 	},
 	variants: ["hover", "focus"],
