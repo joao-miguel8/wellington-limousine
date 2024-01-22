15ff1258013ad94e3201fde0fcff22c5109679cd
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-22 14:48:18 -0500

    - Change Nav menu markup/ styling
    - Change menuItems data
    - Add  isMenuClosed, setIsMenuClosed props to Nav.tsx
    - Add header in App.tsx for Nav.tsx
    - Add isMenuClosed and setIsMenuClosed in App.tsx

--- wellington-limousine/src/App.tsx
+++ wellington-limousine/src/App.tsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 import Nav from "./components/Nav";
 import HeroSection from "./components/hero-section/HeroSection";
+import { MdMenu } from "react-icons/md";
 
 function App() {
+	const [isMenuClosed, setIsMenuClosed] = useState(false);
 	return (
 		<>
-			<Nav />
-			<HeroSection />
+			<header className="h-16 w-full top-0 fixed flex items-center justify-end">
+				<button onClick={() => setIsMenuClosed(true)} className="mr-8 text-white hover:text-[#D7B65C] hover:duration-200">
+					<MdMenu size={"1.8rem"} />
+				</button>
+			</header>
+			<Nav isMenuClosed={isMenuClosed} setIsMenuClosed={setIsMenuClosed} />
+			{/* <HeroSection /> */}
 		</>
 	);
 }
--- wellington-limousine/src/components/Nav.tsx
+++ wellington-limousine/src/components/Nav.tsx
@@ -2,36 +2,32 @@ import { MdOutlineMail } from "react-icons/md";
 import { MdOutlineQuestionAnswer } from "react-icons/md";
 import { GoQuestion } from "react-icons/go";
 import { MdOutlineDesignServices } from "react-icons/md";
+import { IoIosCloseCircleOutline } from "react-icons/io";
 
-function Nav() {
-	const menuIconStyling = {
-		size: "1.4rem",
-		color: "#ffff",
-	};
-	const { size, color } = menuIconStyling;
-
-	const menuItems = [
-		{ name: "About", icon: <GoQuestion size={size} color={color} /> },
-		{ name: "Services", icon: <MdOutlineDesignServices size={size} color={color} /> },
-		{ name: "Reviews", icon: <MdOutlineQuestionAnswer size={size} color={color} /> },
-		{ name: "Contact", icon: <MdOutlineMail size={size} color={color} /> },
-	];
+function Nav({ isMenuClosed, setIsMenuClosed }: { isMenuClosed: boolean; setIsMenuClosed: (boolVal: boolean) => void }) {
+	const menuItems = ["About", "Services", "Reviews", "Contact"];
 
 	return (
-		<div className="flex w-full fixed bottom-0 sm:bottom-auto sm:top-2">
-			<nav className="mx-auto p-2 w-full flex justify-center  bg-gray-800 sm:w-[400px] sm:rounded-full ">
-				<ul className="px-10 w-full flex justify-between items-center max-[400px]:py-2">
-					{menuItems.map(item => {
-						return (
-							<li key={item.name} className="flex flex-col items-center">
-								{item.icon}
-								<span className="text-12 text-white max-[400px]:hidden ">{item.name}</span>
-							</li>
-						);
-					})}
-				</ul>
-			</nav>
-		</div>
+		<>
+			<div className={`${!isMenuClosed ? "-translate-y-full" : "translate-y-0"} ease-in-out transition duration-500 absolute z-100 flex w-full h-screen bg-[#171717]`}>
+				<nav className=" w-full h-full flex flex-col justify-center">
+					<button onClick={() => setIsMenuClosed(false)} className="absolute top-6 right-6 hover:text-[#D7B65C] text-white hover:duration-200">
+						<IoIosCloseCircleOutline size={"2.4rem"} />
+					</button>
+					<ul className="flex flex-col gap-6 items-center justify-start w-fit mx-auto">
+						{menuItems.map(item => {
+							return (
+								<li key={item}>
+									<a href="#">
+										<p className="font-mulish text-28 font-black sm:text-40 text-white hover:text-[#D7B65C] duration-500 hover:scale-75">{item}</p>
+									</a>
+								</li>
+							);
+						})}
+					</ul>
+				</nav>
+			</div>
+		</>
 	);
 }
 
