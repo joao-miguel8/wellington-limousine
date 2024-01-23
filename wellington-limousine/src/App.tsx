c7ea5ebddd41019acde9c48d65a0e0705ab1048c
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-23 14:10:06 -0500

    :sparkles: Create CustomerTestimonials.tsx markup / styling
    - Add Swiper to CustomerTestimonials.tsx
    - Update npm packages

--- wellington-limousine/src/App.tsx
+++ wellington-limousine/src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import "./App.css";
-import Header from "./components/header/Header";
-import NewClientForm from "./components/new-client-form/NewClientForm";
-import HeroSection from "./components/hero-section/HeroSection";
 import paralaxImg from "./assets/luxury-car-img.jpg";
 import paralaxImg2 from "./assets/client-in-backseat.jpg";
+import HeroSection from "./components/hero-section/HeroSection";
+import Header from "./components/header/Header";
+import NewClientForm from "./components/new-client-form/NewClientForm";
 import BenefitsSection from "./components/benefits-section/BenefitsSection";
+import CustomerTestimonials from "./components/customer-testimonials/CustomerTestimonials";
 
 function App() {
 	const [isMenuClosed, setIsMenuClosed] = useState(false);
@@ -15,12 +16,15 @@ function App() {
 			<Header isMenuClosed={isMenuClosed} setIsMenuClosed={setIsMenuClosed} />
 			{/* <HeroSection /> */}
 			<NewClientForm />
-			{/* PARALAX IMG SECTION */}
+			{/* PARALLAX IMG SECTION */}
 			<div style={{ backgroundImage: `url(${paralaxImg})` }} className="mb-[100px] h-[50rem] w-full object-fit bg-no-repeat bg-cover  flex justify-center bg-center align-center bg-fixed">
 				<h2 className="mx-4 mt-20 font-taviraj m-auto max-w-[600px] text-34 min-[500px]:text-50 text-center text-white ">Lorem ipsum dolor sit amet consectetur </h2>
 			</div>
 			{/* BENEFITS SECTION */}
 			<BenefitsSection />
+
+			{/* REVIEWS SECTION */}
+			<CustomerTestimonials />
 		</>
 	);
 }
