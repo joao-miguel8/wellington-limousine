76bee041f9a941a92e82ea8afe3a9cfe231d7f06
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-23 08:49:36 -0500

    - Add paralax Image section
    - Add luxury-car-img.jpg image
    - Add client-in-backseat.jpg image

--- wellington-limousine/src/App.tsx
+++ wellington-limousine/src/App.tsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/header/Header";
 import NewClientForm from "./components/new-client-form/NewClientForm";
+import HeroSection from "./components/hero-section/HeroSection";
+import paralaxImg from "./assets/luxury-car-img.jpg";
+import paralaxImg2 from "./assets/client-in-backseat.jpg";
 
 function App() {
 	const [isMenuClosed, setIsMenuClosed] = useState(false);
@@ -10,6 +13,10 @@ function App() {
 			<Header isMenuClosed={isMenuClosed} setIsMenuClosed={setIsMenuClosed} />
 			{/* <HeroSection /> */}
 			<NewClientForm />
+
+			<div style={{ backgroundImage: `url(${paralaxImg})` }} className="mb-[2000px] h-[50rem] w-full object-fit bg-no-repeat bg-cover  flex justify-center bg-center align-center bg-fixed">
+				<h2 className=" mt-20 font-taviraj m-auto max-w-[600px] text-50 text-center text-white ">Lorem ipsum dolor sit amet consectetur </h2>
+			</div>
 		</>
 	);
 }
