06d160ccc9c96aace2b0680396e84bc441866fa9
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-23 11:43:23 -0500

    Feat: create BenefitsSection.tsx markup, styling and media queries
    - Add cartoon-driver-img
    - Fix parallax image text by adding a media query

--- wellington-limousine/src/App.tsx
+++ wellington-limousine/src/App.tsx
@@ -5,18 +5,22 @@ import NewClientForm from "./components/new-client-form/NewClientForm";
 import HeroSection from "./components/hero-section/HeroSection";
 import paralaxImg from "./assets/luxury-car-img.jpg";
 import paralaxImg2 from "./assets/client-in-backseat.jpg";
+import BenefitsSection from "./components/benefits-section/BenefitsSection";
 
 function App() {
 	const [isMenuClosed, setIsMenuClosed] = useState(false);
+
 	return (
 		<>
 			<Header isMenuClosed={isMenuClosed} setIsMenuClosed={setIsMenuClosed} />
 			{/* <HeroSection /> */}
 			<NewClientForm />
-
-			<div style={{ backgroundImage: `url(${paralaxImg})` }} className="mb-[2000px] h-[50rem] w-full object-fit bg-no-repeat bg-cover  flex justify-center bg-center align-center bg-fixed">
-				<h2 className=" mt-20 font-taviraj m-auto max-w-[600px] text-50 text-center text-white ">Lorem ipsum dolor sit amet consectetur </h2>
+			{/* PARALAX IMG SECTION */}
+			<div style={{ backgroundImage: `url(${paralaxImg})` }} className="mb-[100px] h-[50rem] w-full object-fit bg-no-repeat bg-cover  flex justify-center bg-center align-center bg-fixed">
+				<h2 className="mx-4 mt-20 font-taviraj m-auto max-w-[600px] text-34 min-[500px]:text-50 text-center text-white ">Lorem ipsum dolor sit amet consectetur </h2>
 			</div>
+			{/* BENEFITS SECTION */}
+			<BenefitsSection />
 		</>
 	);
 }
---
+++ wellington-limousine/src/components/benefits-section/BenefitsSection.tsx
@@ -0,0 +1,60 @@
+import benefitsImg from "../../assets/cartoon-driver-img.jpg";
+import { FaCarSide } from "react-icons/fa";
+
+function BenefitsSection() {
+	const benefitsData = [
+		{
+			icon: <FaCarSide size={"2.8rem"} color="#D7B65C" />,
+			description: "Licensed and Insured",
+		},
+		{
+			icon: <FaCarSide size={"2.8rem"} color="#D7B65C" />,
+			description: "Booked by Appointments only",
+		},
+		{
+			icon: <FaCarSide size={"2.8rem"} color="#D7B65C" />,
+			description: "Frequently serviced, clean vehicles.",
+		},
+		{
+			icon: <FaCarSide size={"2.8rem"} color="#D7B65C" />,
+			description: "Professionally Trained Drivers",
+		},
+		{
+			icon: <FaCarSide size={"2.8rem"} color="#D7B65C" />,
+			description: "Shopping, hospitals, Special Events",
+		},
+		{
+			icon: <FaCarSide size={"2.8rem"} color="#D7B65C" />,
+			description: "Airport pickup and drop off",
+		},
+	];
+
+	return (
+		<div className="bg-white">
+			<div className="px-2 mx-auto max-w-[350px]">
+				<h4 className="mx-auto font-taviraj font-medium text-center color-[#171717] text-22 min-[350px]:text-28 ">
+					Benefits <span className="text-[#D7B65C]">ipsum dolor </span>
+					malaga lada
+				</h4>
+				<p className="mt-4 font-taviraj text-center font-light italic">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+			</div>
+			<img src={benefitsImg} alt="#" className="mx-auto mt-12 w-full min-[400px]:w-[40%] " />
+
+			{/* BENEFITS LIST */}
+			<div className="mt-12 p-4 justify-center gap-4 bg-[#171717]">
+				<div className="mx-auto p-8 max-w-[600px] flex flex-wrap  justify-center gap-8 min-[404px]:justify-between">
+					{benefitsData.map(benefit => {
+						return (
+							<div className="flex-33% max-w-[120px] flex flex-col items-center max-[400px]:mb-8">
+								{benefit.icon}
+								<span className="mt-4 font-taviraj text-center font-medium text-white">{benefit.description}</span>
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default BenefitsSection;
