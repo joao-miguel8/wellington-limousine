7d4fbc2549f32a1b4ad3c192e058dfbd5bed1387
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-24 18:19:07 -0500

    - Add hide nav logo in large screen

--- wellington-limousine/src/components/Nav.tsx
+++ wellington-limousine/src/components/Nav.tsx
@@ -13,7 +13,7 @@ function Nav({ isMenuClosed, setIsMenuClosed }: { isMenuClosed: boolean; setIsMe
 			<div className={`${!isMenuClosed ? "-translate-x-full" : "translate-x-0"} ease-in-out transition duration-500 top-0 fixed z-50 flex w-full min-[400px]:w-[310px] h-screen bg-[#171717]`}>
 				<nav className="sm:ml-8 w-full h-full flex flex-col ">
 					<div className="mt-8 mx-8 pb-4 flex justify-between items-center border-b-[0.5px] border-[#e8e8e8]">
-						<img src={logo} alt="wellington limousine logo" className="w-32 sm:w-36" />
+						<img src={logo} alt="wellington limousine logo" className="w-32 sm:w-36 min-[400px]:opacity-0" />
 						<button onClick={() => setIsMenuClosed(false)} className="top-4 right-8 hover:text-[#D7B65C] text-white hover:duration-200">
 							<IoIosCloseCircleOutline size={"2.0rem"} />
 						</button>
