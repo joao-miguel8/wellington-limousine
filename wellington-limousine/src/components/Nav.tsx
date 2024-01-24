f8905543faaf1a062285c7d0fbd914fcfe8a3584
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-24 09:45:24 -0500

    - Chang markup and styling for Nav.tsx
    - Add wellingtonLimousineLogo image
    - Add div to wrap image and close button to create a top section
    - Change font size for menu items

--- wellington-limousine/src/components/Nav.tsx
+++ wellington-limousine/src/components/Nav.tsx
@@ -3,6 +3,7 @@ import { MdOutlineQuestionAnswer } from "react-icons/md";
 import { GoQuestion } from "react-icons/go";
 import { MdOutlineDesignServices } from "react-icons/md";
 import { IoIosCloseCircleOutline } from "react-icons/io";
+import logo from "../assets/wellingtonLimousineLogo.png";
 
 function Nav({ isMenuClosed, setIsMenuClosed }: { isMenuClosed: boolean; setIsMenuClosed: (boolVal: boolean) => void }) {
 	const menuItems = ["About", "Services", "Reviews", "Contact"];
@@ -10,16 +11,19 @@ function Nav({ isMenuClosed, setIsMenuClosed }: { isMenuClosed: boolean; setIsMe
 	return (
 		<>
 			<div className={`${!isMenuClosed ? "-translate-x-full" : "translate-x-0"} ease-in-out transition duration-500 top-0 fixed z-50 flex w-full h-screen bg-[#171717]`}>
-				<nav className=" w-full h-full flex flex-col justify-center">
-					<button onClick={() => setIsMenuClosed(false)} className="absolute top-4 right-8 hover:text-[#D7B65C] text-white hover:duration-200">
-						<IoIosCloseCircleOutline size={"2.4rem"} />
-					</button>
-					<ul className="flex flex-col gap-6 items-center justify-start w-fit mx-auto">
+				<nav className="sm:ml-8 w-full h-full flex flex-col ">
+					<div className="mt-8 mx-8 pb-4 flex justify-between items-center border-b-2">
+						<img src={logo} alt="wellington limousine logo" className="w-32 sm:w-52" />
+						<button onClick={() => setIsMenuClosed(false)} className="top-4 right-8 hover:text-[#D7B65C] text-white hover:duration-200">
+							<IoIosCloseCircleOutline size={"2.4rem"} />
+						</button>
+					</div>
+					<ul className="mx-auto min-[250px]:ml-10 mt-10 w-fit">
 						{menuItems.map(item => {
 							return (
 								<li key={item}>
 									<a href="#">
-										<p className="font-mulish text-28 font-black sm:text-40 text-white hover:text-[#D7B65C] duration-500 hover:scale-75">{item}</p>
+										<p className="mb-10 font-mulish text-28 font-black sm:text-34 text-white hover:text-[#D7B65C] duration-500 hover:scale-75">{item}</p>
 									</a>
 								</li>
 							);
