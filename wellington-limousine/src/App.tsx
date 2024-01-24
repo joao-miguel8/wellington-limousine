b3b75022f54ef3a1bed6c2ddccdfc8851dba05f1
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-23 20:12:34 -0500

    Style: change margin bottom

--- wellington-limousine/src/App.tsx
+++ wellington-limousine/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
 			<NewClientForm />
 			{/* PARALLAX IMG SECTION */}
 			{/* style={{ backgroundImage: `url(${parallaxImgMobile})` }} */}
-			<div className="mb-40 relative">
+			<div className="mb-28 relative">
 				<div className={`h-[30rem] w-full object-fit bg-no-repeat bg-cover flex justify-center bg-center align-center bg-fixed brightness-50 bg-[url(src/assets/client-in-backseat-mobile.jpg)] min-[350px]:bg-[url(src/assets/client-in-backseat.jpg)]`}></div>
 				<div className="w-full absolute top-1/2 -translate-y-1/2 z-20">
 					<h2 className="px-2 mx-auto mb-30 font-taviraj max-w-[600px] text-30 min-[400px]:text-38 min-[800px]:text-50 text-center text-white">Lorem ipsum dolor sit amet consectetur </h2>
