74e4a8b3d37a00ba171c1f79c1a58c11f7c7ce92
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-24 12:31:37 -0500

    - Add sub title to NewClientForm.tsx and change div to section

--- wellington-limousine/src/components/new-client-form/NewClientForm.tsx
+++ wellington-limousine/src/components/new-client-form/NewClientForm.tsx
@@ -14,8 +14,11 @@ function NewClientForm() {
 	});
 
 	return (
-		<div className="mx-auto pb-10 pt-6 mt-[70px] rounded-sm max-w-[700px]">
-			<h4 className="mx-auto font-taviraj font-medium text-center color-[#171717] text-22 min-[350px]:text-28 max-w-[350px]">Notify us by email to be a potential client</h4>
+		<section className="mt-[14rem] mx-auto pb-10 pt-6 rounded-sm max-w-[700px]">
+			<div className="flex flex-col items-center">
+				<span className="font-taviraj text-16 font-semibold italic text-[#D7B65C] border-b border-[#D7B65C]">Sign up</span>
+				<h4 className="mt-4 mx-auto font-taviraj font-medium text-center color-[#171717] text-22 min-[350px]:text-28 max-w-[350px]">Notify us by email to be a potential client</h4>
+			</div>
 			<form action="" className="mx-2 p-8 mt-10 bg-[#F8F8F8]">
 				<div className="flex flex-col w-full gap-6 ">
 					<div className="flex flex-col font-mulish">
@@ -37,7 +40,7 @@ function NewClientForm() {
 					</div>
 				</div>
 			</form>
-		</div>
+		</section>
 	);
 }
 
