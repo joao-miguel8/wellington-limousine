0d30b931967f58eb7b1e0ee227ce5f2a889bee1e
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-24 16:36:57 -0500

    :lipstick: Change nav styles

--- wellington-limousine/src/components/Nav.tsx
+++ wellington-limousine/src/components/Nav.tsx
@@ -10,20 +10,20 @@ function Nav({ isMenuClosed, setIsMenuClosed }: { isMenuClosed: boolean; setIsMe
 
 	return (
 		<>
-			<div className={`${!isMenuClosed ? "-translate-x-full" : "translate-x-0"} ease-in-out transition duration-500 top-0 fixed z-50 flex w-full h-screen bg-[#171717]`}>
+			<div className={`${!isMenuClosed ? "-translate-x-full" : "translate-x-0"} ease-in-out transition duration-500 top-0 fixed z-50 flex w-full min-[400px]:w-[310px] h-screen bg-[#171717]`}>
 				<nav className="sm:ml-8 w-full h-full flex flex-col ">
-					<div className="mt-8 mx-8 pb-4 flex justify-between items-center border-b-2">
-						<img src={logo} alt="wellington limousine logo" className="w-32 sm:w-52" />
+					<div className="mt-8 mx-8 pb-4 flex justify-between items-center border-b-[0.5px] border-[#e8e8e8]">
+						<img src={logo} alt="wellington limousine logo" className="w-32 sm:w-36" />
 						<button onClick={() => setIsMenuClosed(false)} className="top-4 right-8 hover:text-[#D7B65C] text-white hover:duration-200">
-							<IoIosCloseCircleOutline size={"2.4rem"} />
+							<IoIosCloseCircleOutline size={"2.0rem"} />
 						</button>
 					</div>
-					<ul className="mx-auto min-[250px]:ml-10 mt-10 w-fit">
+					<ul className="ml-10 mt-10 w-fit">
 						{menuItems.map(item => {
 							return (
 								<li key={item}>
 									<a href={`#${item}`}>
-										<p className="mb-10 font-mulish text-28 font-black sm:text-34 text-white hover:text-[#D7B65C] duration-500 hover:scale-75">{item}</p>
+										<p className="mb-10 font-mulish text-18 font-black text-white hover:text-[#D7B65C] duration-500 hover:scale-75">{item}</p>
 									</a>
 								</li>
 							);
