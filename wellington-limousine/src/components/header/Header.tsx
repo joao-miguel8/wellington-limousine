e7356726f2205949192480c1d610694b6c04bba6
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-24 17:06:25 -0500

    - Change reverse img and menu icon button position

--- wellington-limousine/src/components/header/Header.tsx
+++ wellington-limousine/src/components/header/Header.tsx
@@ -5,11 +5,11 @@ import logo from "../../assets/wellingtonLimousineLogo.png";
 function Header({ isMenuClosed, setIsMenuClosed }: { isMenuClosed: boolean; setIsMenuClosed: (boolVal: boolean) => void }) {
 	return (
 		<div>
-			<header className="pl-2 sm:pl-10 h-[5rem] w-full z-20 top-0 fixed flex items-center justify-between bg-[#171717]">
-				<img src={logo} alt="wellington limousine logo" className="w-40" />
+			<header className="px-4 sm:pl-10 h-[5rem] w-full z-20 top-0 fixed flex items-center justify-between bg-[#171717]">
 				<button onClick={() => setIsMenuClosed(true)} className="mr-8 text-white hover:text-[#D7B65C] hover:duration-200">
 					<MdMenu size={"1.8rem"} />
 				</button>
+				<img src={logo} alt="wellington limousine logo" className="w-40" />
 			</header>
 			<Nav isMenuClosed={isMenuClosed} setIsMenuClosed={setIsMenuClosed} />
 		</div>
