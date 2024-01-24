d05b8d228556dde55749eb8756ec74371e561bbe
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-24 12:16:53 -0500

    - Add logo image to Header.tsx and change styling

--- wellington-limousine/src/components/header/Header.tsx
+++ wellington-limousine/src/components/header/Header.tsx
@@ -1,11 +1,12 @@
-import React from "react";
 import Nav from "../Nav";
 import { MdMenu } from "react-icons/md";
+import logo from "../../assets/wellingtonLimousineLogo.png";
 
 function Header({ isMenuClosed, setIsMenuClosed }: { isMenuClosed: boolean; setIsMenuClosed: (boolVal: boolean) => void }) {
 	return (
 		<div>
-			<header className="h-16 w-full top-0 fixed flex items-center justify-end bg-[#171717]">
+			<header className="pl-2 sm:pl-10 h-[5rem] w-full z-20 top-0 fixed flex items-center justify-between bg-[#171717]">
+				<img src={logo} alt="wellington limousine logo" className="w-40" />
 				<button onClick={() => setIsMenuClosed(true)} className="mr-8 text-white hover:text-[#D7B65C] hover:duration-200">
 					<MdMenu size={"1.8rem"} />
 				</button>
