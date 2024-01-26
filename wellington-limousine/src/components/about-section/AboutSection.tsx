ee998dea163a75c6d4644f7ad726dac3ca608611
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-26 11:38:31 -0500

    - Change AboutSection.tsx text content

--- wellington-limousine/src/components/about-section/AboutSection.tsx
+++ wellington-limousine/src/components/about-section/AboutSection.tsx
@@ -8,9 +8,14 @@ function AboutSection() {
 				Discover <span className="text-[#D7B65C]">our story</span>
 			</h2>
 			<p className="mt-4 text-18 font-mulish">
-				Lorem sit Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis repudiandae, quaerat sit incidunt enim voluptate quibusdam quos consequuntur quas molestiae. amet consectetur adipisicing elit. Doloribus, facere! dolor sit amet, consectetur adipisicing elit. Et, sint officiis,
-				enim ea adipisci sed minima doloremque at pariatur nesciunt alias est dignissimos, nisi distinctio ducimus magni corporis. Tenetur, incidunt?
+				Welcome to Wellington Limousine, your premier chauffeur service in the heart of the NY tristate area. Established in 2017, we have been dedicated to providing an unparalleled experience for our discerning clients.Since our launch, we have taken pride in transforming transportation
+				into an elevated service. Our journey started with a commitment to excellence and a passion for delivering a chauffeur experience that goes beyond expectations.
 			</p>
+			<p className="mt-4 text-18 font-mulish">
+				At Wellington Limousine, we understand that our clients are not just passengers; they are valued partners in our journey. Our business ensures a safe and professional ride every time. We are a personalized Service, your comfort is our priority, and we tailor our services to meet your
+				unique needs. We also value punctuality, we know the importance of time, and we are committed to a prompt and punctual service.
+			</p>
+			<p className="mt-4 text-18 font-mulish">Whether you're heading to a business meeting, special event, or simply need a reliable ride, we are here for you. Join us on this journey of luxury, comfort, and professional service.</p>
 		</section>
 	);
 }
