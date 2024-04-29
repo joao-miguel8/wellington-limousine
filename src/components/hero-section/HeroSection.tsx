c5101a167a1057a1d465de660fab020d6cadeb95
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-04-29 12:32:20 -0400

    - Change HeroSection text content

--- src/components/hero-section/HeroSection.tsx
+++ src/components/hero-section/HeroSection.tsx
@@ -10,27 +10,29 @@ function HeroSection({ handleFormInputFocusScroll }: { handleFormInputFocusScrol
 
 	return (
 		<div className="relative">
-			<Swiper
-				autoplay={{
-					delay: 8000,
-					disableOnInteraction: false,
-				}}
-				modules={[Autoplay]}
-				slidesPerView={1}>
-				{slides.map(slide => {
-					return (
-						<SwiperSlide key={slide}>
-							<div style={{ backgroundImage: `url(${slide})` }} className="mt-[5rem] h-[600px] w-full object-fit bg-center bg-no-repeat bg-cover brightness-50"></div>
-						</SwiperSlide>
-					);
-				})}
-				{/* href="#Contact" */}
-			</Swiper>
-			<div className="max-w-[540px] absolute z-10 bottom-[20%] left-[6%]">
-				<h2 className="font-taviraj text-24 sm:text-36 text-white">Experience our top-notch driving services</h2>
-				<a onClick={() => handleFormInputFocusScroll()} className="mt-8 px-4 py-2 inline-block text-20 font-mulish font-semibold text-[#171717] bg-[#D7B65C] hover:bg-[#dfab1c] hover:scale-[1.1] duration-300 ">
-					Contact us
-				</a>
+			<div>
+				<Swiper
+					autoplay={{
+						delay: 8000,
+						disableOnInteraction: false,
+					}}
+					modules={[Autoplay]}
+					slidesPerView={1}>
+					{slides.map(slide => {
+						return (
+							<SwiperSlide key={slide}>
+								<div style={{ backgroundImage: `url(${slide})` }} className="mt-[5rem] h-[600px] w-full object-fit bg-center bg-no-repeat bg-cover brightness-50"></div>
+							</SwiperSlide>
+						);
+					})}
+					{/* href="#Contact" */}
+				</Swiper>
+				<div className="max-w-[540px] absolute z-10 bottom-[20%] left-[6%]">
+					<h2 className="font-taviraj text-24 sm:text-36 text-white">Contact us for an Affordable Private Chauffeur driver</h2>
+					<a onClick={() => handleFormInputFocusScroll()} className="mt-8 px-4 py-2 inline-block text-20 font-mulish font-semibold text-[#171717] bg-[#D7B65C] hover:bg-[#dfab1c] hover:scale-[1.1] duration-300 ">
+						Contact us
+					</a>
+				</div>
 			</div>
 		</div>
 	);
