4eadb0eb6b6e1cd55143a9653bb056b774bbb79c
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-04-29 12:32:41 -0400

    - Change text

--- src/components/parallax-image-scroll-section/ParallaxImageScrollSection.tsx
+++ src/components/parallax-image-scroll-section/ParallaxImageScrollSection.tsx
@@ -3,7 +3,7 @@ function ParallaxImageScrollSection() {
 		<div className="mb-28 relative">
 			<div className={`h-[30rem] sm:h-[50rem] w-full object-fit bg-no-repeat bg-cover flex justify-center bg-center align-center bg-fixed brightness-50 bg-[url(../src/assets/client-in-backseat-mobile.jpg)] min-[400px]:bg-[url(../src/assets/client-in-backseat.jpg)]`}></div>
 			<div className="w-full absolute top-1/2 -translate-y-1/2 ">
-				<h2 className="px-2 mx-auto mb-30 font-taviraj max-w-[600px] text-30 min-[400px]:text-38 sm:text-50 md:text-62 text-center text-white">Experience top quality comfort</h2>
+				<h2 className="px-2 mx-auto mb-30 font-taviraj max-w-[600px] text-30 min-[400px]:text-38 sm:text-50 md:text-62 text-center text-white">Tavel in Quality and Comfort</h2>
 			</div>
 		</div>
 	);
