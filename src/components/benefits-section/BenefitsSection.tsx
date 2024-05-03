ee0dcd51ffc65dea5afe790faa75244204adda7c
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-05-03 16:19:19 -0400

    - Add styling

--- src/components/benefits-section/BenefitsSection.tsx
+++ src/components/benefits-section/BenefitsSection.tsx
@@ -43,7 +43,7 @@ function BenefitsSection() {
 				<p className="mt-10 font-taviraj text-center font-light italic text-22">Chauffeur Services benefits we provide, Where Luxury Meets Quality</p>
 			</div>
 			{/* BENEFITS LIST */}
-			<div className="gap-4 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">
+			<div className="divide-y divide-solid gap-4 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">
 				{benefitsData.map(benefit => {
 					return (
 						<div key={benefit.title} className="p-4 group flex max-[800px]:flex-col max-[800px]:mt-10 gap-4 items-center max-[400px]:mb-8 ">
