c7c55d502915ea655ec7d47f8349a57453feff7c
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-06-12 11:21:19 -0400

    - Add footer to about section

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import AboutSection from "../components/about-section/AboutSection";
 import Header from "../components/header/Header";
 import Nav from "../components/Nav";
+import Footer from "../components/footer/Footer";
 
 function AboutPage() {
 	const [isMenuClosed, setIsMenuClosed] = useState(false);
@@ -12,10 +13,11 @@ function AboutPage() {
 				<Nav isMenuClosed={isMenuClosed} setIsMenuClosed={setIsMenuClosed} />
 			</Header>
 			<main>
-				<div className="mt-40 mb-60">
+				<div className="mt-40 mb-40">
 					<AboutSection />
 				</div>
 			</main>
+			<Footer />
 		</>
 	);
 }
